7388fa496161cb8bcbe2eb9f1f4821bf2ed74c41
Srinath Sureshkumar <srinathsk18@gmail.com>
2021-08-01 18:56:06 +0530

    Data Update

--- DataUpdate/data/CountyPops.py
+++ DataUpdate/data/CountyPops.py
@@ -5,14 +5,14 @@ from scipy import stats
 import sys
 import re
 
-counties_df = pd.read_csv('DataUpdate/data/COVID-CASES-Counties-DF.txt', sep='\t') 
+counties_df = pd.read_csv('COVID-CASES-Counties-DF.txt', sep='\t') 
 counties_df = counties_df[~counties_df['Admin2'].isin(['Unassigned', 'Out-of-state', 
                                 'Out of AL', 'Out of IL','Out of CO', 'Out of GA',
                                 'Out of HI', 'Out of LA', 'Out of ME', 'Out of MI',
-                                'Out of OK', 'Out of PR', 'Out of TN', 'Out of UT',])]
+                                'Out of OK', 'Out of PR', 'Out of TN', 'Out of UT', 'Out of CH'])]
 
 
-cty_pops = pd.read_excel('DataUpdate/data/co-est2019-annres.xlsx')
+cty_pops = pd.read_excel('co-est2019-annres.xlsx')
 cty_pops = cty_pops[cty_pops['Geographic Area'] != 'United States']
 
 
@@ -62,4 +62,4 @@ cty_pops['Population size'] = cty_pops[2019].values.tolist()
 cty_pops.drop(['Geographic Area'], axis=1, inplace=True)
 cty_pops.drop([2019], axis=1, inplace=True)
 
-cty_pops.to_pickle('DataUpdate/data/County_Pops.pkl')
+cty_pops.to_pickle('County_Pops.pkl')
